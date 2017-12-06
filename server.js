09c5b6a593c8134201d3ada5944d8f1e56c319bc
Cong-Kien Dang <kiendc@gmail.com>
2017-12-05 22:48:40 -0500

    Ignore node_modules in the local Replace res.render() by res.send()

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
-        res.render('index.html', { pageCountMessage : null});
+        res.send('Hello world');
 });
 
 app.use(function(err, req, res, next){
