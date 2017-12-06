0e979021a3429ce8b41bcc47210ac3977a65c145
Cong-Kien Dang <kiendc@gmail.com>
2017-12-05 22:14:10 -0500

    Get template from nodejs-ex of Openshift

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+var express = require('express'),
+    app     = express();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.get('/', function (req, res) {
+        res.render('index.html', { pageCountMessage : null});
+});
+
+app.use(function(err, req, res, next){
+        console.error(err.stack);
+        res.status(500).send('Something bad happened!');
+        });
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
