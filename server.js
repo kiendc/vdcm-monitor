636c5b6f3cda5fe9f56b4305b8cb55ef6a6ab3fb
Cong-Kien Dang <kiendc@gmail.com>
2017-12-06 00:04:31 -0500

    Change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express'),
     app     = express();
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 1908,
 ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
