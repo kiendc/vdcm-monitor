1f70c00fd2e83cb7fbcb15207b56701b17edce6f
kiendc <kiendc@gmail.com>
2017-12-07 15:22:46 -0500

    Change the port as convention of Openshift

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
     http = require('http').Server(app),
     io = require('socket.io')(http);
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 1908,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
